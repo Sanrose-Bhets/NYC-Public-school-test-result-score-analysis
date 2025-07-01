1c7e9359c6ffe86731eb80888e55e7ff2d878cba
Sanrose-Bhets <sanrosebhetwal@gmail.com>
2025-07-01 19:34:02 +0545

    Completed Best Maths results

--- analysis.py
+++ analysis.py
@@ -5,5 +5,7 @@ school = pd.read_csv("schools.csv")
 schooling = pd.DataFrame(school)
 
 precentage = 800 * 0.8
-best_math_schools = schooling[schooling["average_math"]>precentage]
+#subsetting the columns
+desiredcolumns = schooling[["school_name","average_math"]]
+best_math_schools = desiredcolumns[desiredcolumns["average_math"]>precentage].sort_values("average_math", ascending=False)
 print(best_math_schools)
