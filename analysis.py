5b63ccc67ab37dba3b634549a32eba839f79d1ae
Sanrose-Bhets <sanrosebhetwal@gmail.com>
2025-07-03 12:21:24 +0545

    almost finish std

--- analysis.py
+++ analysis.py
@@ -4,18 +4,28 @@ school = pd.read_csv("schools.csv")
 
 schooling = pd.DataFrame(school)
 
+# Best maths Results Calculations
 precentage = 800 * 0.8
-#subsetting the columns
-#Best maths Results Calculations
-precentage = 800 * 0.8
-desiredcolumns = schooling[["school_name","average_math"]]
-best_math_schools = desiredcolumns[desiredcolumns["average_math"]>precentage].sort_values("average_math", ascending=False)
+# Slicing/subsetting desired columns to make the below code much more understanding
+desiredcolumns = schooling[["school_name", "average_math"]]
+best_math_schools = desiredcolumns[desiredcolumns["average_math"] > precentage].sort_values("average_math", ascending=False)
 print(best_math_schools)
 
-#Top 10 performing schooling in SATs
+# Top 10 performing schooling in SATs
 schooling["total_SAT"] = schooling["average_math"] + schooling["average_reading"] + schooling["average_writing"]
 
-
-top_SAT=schooling.sort_values("total_SAT",ascending=False)
-top_10_schools = top_SAT.iloc[0:10,:]
+top_SAT = schooling.sort_values("total_SAT", ascending=False)
+columnsdesired = top_SAT[["school_name", "total_SAT"]]
+top_10_schools = columnsdesired.iloc[0:10, :]
 print(top_10_schools)
+
+# Calculating the Standard Deviation of all the schools Numeric Values
+stats_borrowed = schooling.groupby("borough").agg(
+    num_schools=('total_SAT', 'count'),
+    average_SAT=('total_SAT', 'mean'),
+    std_SAT=('total_SAT', 'std')
+).round(2).sort_values("std_SAT", ascending=False)
+
+largest_std_dev = stats_borrowed.iloc[0]
+
+print(largest_std_dev)
