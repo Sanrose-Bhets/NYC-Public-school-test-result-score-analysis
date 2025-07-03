6cea61d59f8b5c4f395e568db85c6f2bc0ea721a
Sanrose-Bhets <sanrosebhetwal@gmail.com>
2025-07-03 10:35:02 +0545

    completed top 19 SAT schools

--- analysis.py
+++ analysis.py
@@ -6,6 +6,16 @@ schooling = pd.DataFrame(school)
 
 precentage = 800 * 0.8
 #subsetting the columns
+#Best maths Results Calculations
+precentage = 800 * 0.8
 desiredcolumns = schooling[["school_name","average_math"]]
 best_math_schools = desiredcolumns[desiredcolumns["average_math"]>precentage].sort_values("average_math", ascending=False)
 print(best_math_schools)
+
+#Top 10 performing schooling in SATs
+schooling["total_SAT"] = schooling["average_math"] + schooling["average_reading"] + schooling["average_writing"]
+
+
+top_SAT=schooling.sort_values("total_SAT",ascending=False)
+top_10_schools = top_SAT.iloc[0:10,:]
+print(top_10_schools)
